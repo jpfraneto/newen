6dd79b89909685dbe40153e70561101ad997d005
jpfraneto <jpfraneto@gmail.com>
2023-04-24 12:38:50 -0400

    aloja

--- pages/invitation/[sadhanaId].js
+++ pages/invitation/[sadhanaId].js
@@ -33,7 +33,7 @@ const SadhanaInvitation = ({ sadhana }) => {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          sadhanaId: 1,
+          sadhanaId: router.query.sadhanaId,
         }),
       });
 
