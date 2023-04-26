6abedeacec76db0dc43811eab31e3243cf3c08cc
jpfraneto <jpfraneto@gmail.com>
2023-04-26 00:01:43 -0400

    edited text

--- pages/termsandconditions.js
+++ pages/termsandconditions.js
@@ -33,10 +33,11 @@ export default function Home() {
           >
             <ol className='flex flex-col space-y-4'>
               <li>
-                1. I, jp, commit to give a prize of 250 $APE to choose randomly
-                a person that finished any kind of challenge submitted by
-                him/her/it self in sadhana between today, tuesday april 25th and
-                the 15th of may.
+                1. I, jp, commit to give a prize of 250 $APE to a randomnly
+                choosen person. The pool of options will be gathered from all of
+                those users of this platform that finished any kind of challenge
+                submitted by him/her/it self in it between today, tuesday april
+                25th and the 15th of may.
               </li>
               <li>
                 2. For me to be able to have money to give that price, I need to
@@ -56,10 +57,11 @@ export default function Home() {
                 the sun, or whatever. The important thing is that you do it.{' '}
               </li>
               <li>
-                5. The crazier, the better. 3 days is the bottom limit, but it
-                can be for as long as you want. But please have in mind that you
-                need to have done it for more than 3 days the 15th of may, so
-                that I can add it to the pool for this prize.
+                5. The crazier, the better. It won&apos;t give you more chances
+                of winning, but it will be funnier. 3 days is the bottom limit,
+                but it can be for as long as you want. But please have in mind
+                that you need to have done it for more than 3 days the 15th of
+                may, so that I can add it to the pool for this prize.
               </li>
               <li>
                 6. Remember, if I don&apos;t win the contest, I won&apos;t be
@@ -70,6 +72,23 @@ export default function Home() {
                 your help on it. And that means coming up with a challenge, and
                 just doing it! The transformative power of this thing is real,
                 and I want to invite you to experience it on your own world.
+              </li>
+              <li>
+                8. I hope that you are able to see that what is going to happen
+                here will be awesome. A lot of people will benefit from that,
+                because the world right now is built upon people that think they
+                are not creative. They have that door closed. And consistency
+                opens up that door, because it is the invitation to realize that
+                creativity is not a characteristic of &apos;some humans&apos;.
+                It doesn&apos;t work like that. Creativity is a consequence of
+                showing up, doing the thing, and just sharing what you came up
+                with. It is a process, it is what comes at the other side of
+                showing up. And this app will help you show up. You will help
+                you show up, because you will realize the deep impact that it
+                will have in your life. And you will talk to others. And the
+                internet is a funny place. It will go viral. People will spread
+                the word. And we will all have fun together. Something big is
+                coming, and you are at the genesis of it.
               </li>
             </ol>
           </div>
