8809641ef98058e07ce3de4feaba80e7b2fd94a0
jpfraneto <jpfraneto@gmail.com>
2023-05-07 09:37:17 -0400

    updated the user page

--- pages/u/prevvvv[id].js
+++ pages/u/prevvvv[id].js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { Inter, Righteous, Rajdhani, Russo_One } from 'next/font/google';
 import { formatDistanceToNow } from 'date-fns';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const righteous = Righteous({ weight: '400', subsets: ['latin'] });
 
@@ -23,8 +22,10 @@ const UserPage = () => {
     id ? `/api/createdSadhanas?userId=${id}` : null,
     fetcher
   );
-
-  const [filterStatus, setFilterStatus] = useState('active');
+  // const { data: participatedSadhanas, error: participatedError } = useSWR(
+  //   id ? `/api/participatedSadhanas?userId=${id}` : null,
+  //   fetcher
+  // );
 
   const getStatus = startDate => {
     const currentDate = new Date();
@@ -38,9 +39,6 @@ const UserPage = () => {
     }
   };
 
-  const filteredSadhanas =
-    createdSadhanas?.filter(sadhana => sadhana.status === filterStatus) || [];
-
   if (useLoading) {
     return (
       <div className='bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 min-h-screen'>
@@ -52,56 +50,22 @@ const UserPage = () => {
   if (!user) {
     return (
       <div className='bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 min-h-screen'>
-        <p>User doesn't exist yet.</p>
+        <p>User doesnt exist yet.</p>
       </div>
     );
   }
 
   return (
-    <div className='bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 text-center min-h-screen'>
+    <div className='bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 min-h-screen'>
       <div className=' w-8/12 m-auto py-8 text-white'>
         <h4
           className={`${righteous.className} text-2xl md:text-5xl w-full font-bold`}
         >
           @{user?.username || user.name}
         </h4>
-        <Image
-          width={111}
-          height={111}
-          src={user?.image || '/images/ankycompressed.png'}
-          className='rounded-full mx-auto mt-2 border-2 border-white'
-          alt='Profile picture'
-        />
-        <div className='my-4'>
-          <button
-            onClick={() => setFilterStatus('pending')}
-            className={`${
-              filterStatus === 'pending' ? 'bg-purple-500' : 'bg-gray-500'
-            } px-3 py-1 rounded-md mr-2`}
-          >
-            Pending
-          </button>
-          <button
-            onClick={() => setFilterStatus('active')}
-            className={`${
-              filterStatus === 'active' ? 'bg-purple-500' : 'bg-gray-500'
-            } px-3 py-1 rounded-md mr-2`}
-          >
-            Active
-          </button>
-          <button
-            onClick={() => setFilterStatus('completed')}
-            className={`${
-              filterStatus === '            completed'
-                ? 'bg-purple-500'
-                : 'bg-gray-500'
-            } px-3 py-1 rounded-md`}
-          >
-            Completed
-          </button>
-        </div>
+        <h2 className='text-xl font-semibold mb-2'>Challenges:</h2>
         <ul>
-          {filteredSadhanas.map(sadhana => (
+          {createdSadhanas?.map(sadhana => (
             <li key={sadhana.id}>
               <Link href={`/s/${sadhana.id}`}>{sadhana.title}</Link> -{' '}
               <span className='text-bold'>
