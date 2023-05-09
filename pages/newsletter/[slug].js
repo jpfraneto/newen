937924ccf549d6a11865afcf85a45e9c09407a4b
jpfraneto <jpfraneto@gmail.com>
2023-05-09 14:49:25 -0400

    ahora si

--- pages/newsletter/[slug].js
+++ pages/newsletter/[slug].js
@@ -8,8 +8,9 @@ import Link from 'next/link';
 import { remark } from 'remark';
 import { html } from 'remark-html';
 
-export default function PostPage({ post: { frontmatter, content, slug } }) {
+export default function PostPage({ post }) {
   const router = useRouter();
+  console.log('the post is: ', post);
 
   if (router.isFallback) {
     return <div>Loading...</div>;
@@ -21,22 +22,22 @@ export default function PostPage({ post: { frontmatter, content, slug } }) {
   return (
     <Layout>
       <div className='max-w-2xl mx-auto px-6'>
-        <h1 className='text-4xl font-bold mb-4'>{frontmatter.title}</h1>
+        <h1 className='text-4xl font-bold mb-4'>{post.frontmatter.title}</h1>
         <Image
-          src={frontmatter.image}
-          alt={frontmatter.title}
+          src={post.frontmatter.image}
+          alt={post.frontmatter.title}
           className='mx-auto rounded-xl'
           width={333}
           height={222}
         />
         <p className='text-gray-200 mb-8'>
-          {frontmatter.date} -{' '}
+          {post.frontmatter.date} -{' '}
           <Link href={`/u/clgtinf7j0000js08w6041qqk`}>
-            @{frontmatter.author}
+            @{post.frontmatter.author}
           </Link>
         </p>
         <div className='text-left'>
-          {content.split('\n').map((x, i) => (
+          {post.content.split('\n').map((x, i) => (
             <p className='' key={i}>
               {x}
             </p>
